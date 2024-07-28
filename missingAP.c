efe770bacb0068f013d6b39226e46c772662b1d4
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-07-28 13:00:44 +0530

    print missing element in the ap

---
+++ missingAP.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    int a[5] = {1,3,10,11,13};
+    int ap_diff = a[1] - a[0];
+        for (i = 0; i < 5; i++)  
+    {
+        int expected = a[i] + ap_diff;
+        while (expected < a[i + 1])  
+        {   
+            printf("%d-%d\n",a[i+1] ,expected);
+            expected += ap_diff;
+        }
+    }
+    
+}
