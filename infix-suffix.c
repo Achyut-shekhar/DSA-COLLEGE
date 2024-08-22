73c0e4a5e07d0c04dba598a1ef4b345cd5e65803
Achyut-shekhar <158504706+Achyut-shekhar@users.noreply.github.com>
2024-08-22 11:43:41 +0530

    needs to be corrected

--- infix-suffix.c
+++ infix-suffix.c
@@ -5,16 +5,17 @@ void push(char[],char,int *);
 int pop(char[],int *);
 int pred(char);
 int main()
-{ char stack[max],infix[max],postfix[max],ch,x,i,c,top=-1,pos=0,l;
+{ char stack[max],infix[max],postfix[max],ch,x,i,c,top=0,pos=0,l;
    printf("\n input the infix: -\t");
    fgets(infix,100,stdin);
    l=strlen(infix);
    infix[l-1]=')';
    stack[0]='(';
-   for(i=0;i!="/0";i++)
-   {   infix[i]=c;
-       if(c>="a"&&c<="z"||c>="A"&&c<="Z")
+ while(infix[i]!='\0')
+   {   char c=infix[i];
+       if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
        {  postfix[pos]=c;
+       printf("%c",c);
            pos++;
            }
            else if(c=='(')
@@ -27,42 +28,50 @@ int main()
                 pos++;
                 x=pop(stack,&top);
                 }
+            //pop(stack,&top);
                 }
                 
            else{
                   while(pred(stack[top])>=pred(c))
                   { int x;
-                  x=pop(stack,&top);
+                    x=pop(stack,&top);
+                  
                     postfix[pos]=x;
                     pos++;
                     }
                     push(stack,c,&top);
                   }
-                    
-                  }
-  }
-  
+                    i++;
+   }
+ postfix[pos]='\0';
+ printf("%s",postfix);
+
+}
+
+
   void push(char stack [],char c,int *top)
   {
       
          stack[*top]=c;
          (*top)++;
          }
+         
   int pop(char stack[],int *top)
   {  int x;
       x=stack[*top];
+   (*top)--;
       return x;
       }
       
  int pred(char c)
  {  if(c=='^')
-      return 4;
+      return (4);
     else if(c=='*'||c=='/')
-      return 3;
+      return (3);
     else if(c=='+'||c=='-')
-      return 2;
+      return (2);
       else
-        return 1;
+        return (1);
         }
          
   
