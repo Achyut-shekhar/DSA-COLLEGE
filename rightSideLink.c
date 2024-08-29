5a0aa4ed1111c1dff727cc3412135f74428ed3fc
Achyut-shekhar <158504706+Achyut-shekhar@users.noreply.github.com>
2024-08-29 11:09:04 +0530

    Create rightSideLink.c

---
+++ rightSideLink.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct link{
+ int val;
+ struct link *next;
+ }link;
+int main()
+{  link *head=NULL,*newnode=NULL,*temp=NULL;
+int choice=1;
+while(1)
+{
+   newnode=(link *)malloc(sizeof(link));
+   printf("enter the data\n");
+   scanf("%d",&newnode->val);
+   if(head==NULL)
+   { head=temp=newnode;   //at first node head is NULL and therefore we are saving the first node address
+                          //in head
+   }
+   else
+   { temp->next=newnode;  //after saving the first address in the head we need to save the next node address in the first node and we have the address of first node from temp 
+                          //from this we will not loose first address from head and we can save the next node address with temp
+   temp=newnode;         //at last we will save the address of the newnode in temp
+   }
+   printf("\ndo you want to continue yes-0 no-1");
+   scanf("%d",&choice);
+   if(choice==1)
+   break;
+   }
+   while(head!=0)
+   {printf("%d\t %d\n",head->next,head->val);          //first in first out
+     head=head->next;                                 
+     }
+   }
+   
+   
