9176b4d0acaa870e1d22e57ef0a130f79b890331
Achyut-shekhar <158504706+Achyut-shekhar@users.noreply.github.com>
2024-08-29 11:42:20 +0530

    Create leftSideLink.c

---
+++ leftSideLink.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct link{
+ int val;
+ struct link *next;
+ }link;
+int main()
+{  
+    link *newnode=NULL,*temp=NULL;  //in this we only need newnode which will save the value every new malloc and temp is storing the value for next
+                                    //node 
+    int ch,v;
+    while(1)
+    {
+    newnode=(link *)malloc(sizeof(link));
+    if(newnode!=NULL)
+    { printf("input the value of data\n");
+      scanf("%d",&v);
+      newnode->val=v;
+      newnode->next=temp;
+      temp=newnode;           //here we are storing the address of the newnode in temp so we can store the address of the previous node in the  next
+   }
+   printf("do you want to continue\n");
+   scanf("%d",&ch);
+   if(ch==0)
+   break;
+   }
+   newnode=temp;
+   while(newnode!=NULL)
+   {printf("%d\t%p\n",newnode->val,newnode->next);
+     newnode=newnode->next;
+     }
+     }
+ 
+   
+   
