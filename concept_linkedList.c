9003f40bd42d65873f0065d660db8768f0086f8a
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-08-30 08:40:07 +0530

    updated null in nextnode

--- concept_linkedList.c
+++ concept_linkedList.c
@@ -4,30 +4,40 @@ typedef struct link{
     int val;
     struct link *next;
 }link;
-link makeLink(link *newnode,link *head,link *temp)
-{  int ch;
-    while(1){
-    newnode=(link*)malloc(sizeof(link));
-    printf("\nenter the data");
-    scanf("%d",&newnode->val);
-    if(head==NULL)
-    {
-        head=temp=newnode;
+link *makeLink(link *newnode, link *head, link *temp) {
+    int ch;
+    while (1) {
+        newnode = (link *)malloc(sizeof(link));
+        if (newnode == NULL) {
+            printf("Memory allocation failed\n");
+            break;
+        }
+        printf("Enter the data: ");
+        scanf("%d", &newnode->val);
+        newnode->next = NULL;  // Ensure the new node terminates the list
+
+        if (head == NULL) {
+            head = temp = newnode;  // Initialize the head for the first node
+        } else {
+            temp->next = newnode;  // Link the new node
+            temp = newnode;  // Move the temp pointer to the new node
+        }
+
+        printf("\nDo you want to continue? Yes-1 No-0: ");
+        scanf("%d", &ch);
+        if (ch == 0) {
+            break;  // Break from the loop if the user inputs 0
+        }
     }
-    else{
-        temp->next=newnode;
-        temp=newnode;
-    }
-    printf("\ndo you want to continue\nyes-0\nno-1");
-    scanf("%d",&ch);
-    if(ch==1)
-    break;
-    }
+    return head;  // Return the head of the list
 }
-link display(link *head)
-{
-    while(head!=0)
-   {printf("%d\t %d\n",head->next,head->val);          //first in first out
+
+
+void display(link *head)
+{   printf("\nthe display of the linked lis is:-\n");
+    while(head!=NULL)
+   {
+    printf("\n%p is the address of %d value\n",head->next,head->val);          //first in first out
      head=head->next;                                 
      }
 }
@@ -35,13 +45,14 @@ link display(link *head)
 int main(){
     int choice;
     link *newnode=NULL,*head=NULL,*temp=NULL;
-    printf("input the choice 1-make a linked list\n2-display\n3-insert node at the end\n3-insert node beggning\n4-exit");
-    scanf("%d",&choice);
+    
     do{
+        printf("\n\ninput the choice\n 1-make a linked list\n2-display\n3-insert node at the end\n3-insert node beggning\n4-exit");
+    scanf("%d",&choice);
         switch(choice)
         {
             case 1:
-              makeLink(newnode,head,temp);
+             head= makeLink(newnode,head,temp);
               break;
               case 2:
               display(head);
