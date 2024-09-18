83d369b859bba11e737738d5736add2462901aa2
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-09-18 22:48:11 +0530

    made it menu driven. IMPORTANT

--- doubleLinkascending.c
+++ doubleLinkascending.c
@@ -1,5 +1,6 @@
 // create a double linked list in such a way that the resultant linked list remain in ascending order.
 #include <stdio.h>
+#include <stdlib.h>
 typedef struct doubleLink
 {
   struct doubleLink *prev;
@@ -8,15 +9,17 @@ typedef struct doubleLink
 } dl;
 dl *insertnode(dl *head, int val)
 {
-  dl *newnode = (dl *)malloc(sizeof(dl *));
+  dl *newnode = (dl *)malloc(sizeof(dl));
   newnode->prev = NULL;
   newnode->next = NULL;
   newnode->data = val;
   return newnode;
 }
-dl *createlink(dl *head, int val)
+dl *createlink(dl *head)
 {
-  head = NULL;
+  int val;
+  printf("\ninput the data of the node\n");
+  scanf("%d", &val);
   dl *newnode = insertnode(head, val);
   if (head == NULL)
   {
@@ -31,17 +34,17 @@ dl *createlink(dl *head, int val)
     return newnode;
   }
   // treaversing and finding the right position
-  while (curr->next->data < val)
+  while (curr->next != NULL && curr->next->data < val)//in this last valla check kr raha hai ki jis node peh curr   hai usske next node peh jo data hai voh chota hai ki nahi.
   {
     curr = curr->next;
   }
 
   newnode->next = curr->next;
-  if (curr->next != NULL)
+  if (curr->next != NULL) //checking whether jiss node ke baad lagana hai voh last to nahi 
   {
     curr->next->prev = newnode;
   }
-  curr->next = newnode;
+  curr->next = newnode; //agaar last hai toh sirf yeh step chalengeh
   newnode->prev = curr;
 
   return head;
@@ -58,10 +61,20 @@ void displayList(dl *head)
 }
 int main()
 {
-  int val;
-  printf("\ninput the data of the node\n");
-  scanf("%d", &val);
-
-  dl *head = createlink(head, val);
-  displayList(head);
+  dl *head = NULL;
+  int choice;
+  do
+  {
+    printf("\n1-create the node\n2-display\n");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:
+      head = createlink(head);
+      break;
+    case 2:
+      displayList(head);
+      break;
+    }
+  } while (choice <= 2);
 }
