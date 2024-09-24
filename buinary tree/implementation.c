347979b31d4f656118f14724a68cd326a85746d8
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-09-24 22:49:52 +0530

    added comments

--- buinary tree/implementation.c
+++ buinary tree/implementation.c
@@ -6,6 +6,8 @@ typedef struct binary
   struct binary *left;
   struct binary *right;
 } binary;
+
+//creating a binary tree
 binary *createbinary()
 {
   binary *newnode;
@@ -15,15 +17,18 @@ binary *createbinary()
   if (newnode->data == -1)
     return 0;
   printf("\nenter the left child of %d:", newnode->data);
-  newnode->left = createbinary();
+  newnode->left = createbinary(); //when there will be -1 when this function is calling then it will terminate till this line and next line will be executed;
   printf("\nenter the data of right child of %d:", newnode->data);
   newnode->right = createbinary();
   return newnode;
+}
+void levelOrderTraversal(){
+
 }
 int main()
 {
   binary *root;
   root = createbinary();
 
-  display(root);
+  
 }
