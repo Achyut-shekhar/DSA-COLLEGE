17cdf60af4508e6e0561707657047e058d2302f6
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-11-20 23:37:39 +0530

    termwork

---
+++ termwork3rd/q1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct node
+{
+  int data;
+  struct node *next;
+} node;
+
+node *createlink(node *head)
+{
+  node *temp = NULL, *newnode = NULL;
+  int ch = 0;
+  while (1)
+  {
+    newnode = (node *)malloc(sizeof(node));
+    printf("\ninput the data");
+    scanf("%d", &newnode->data);
+    newnode->next = NULL;
+    if (head == NULL)
+    {
+      head = temp = newnode;
+    }
+    else
+    {
+      temp->next = newnode;
+      temp = newnode;
+    }
+    printf("\nif dont want to continue press 0");
+    scanf("%d", &ch);
+    if (ch == 0)
+      break;
+  }
+  return head;
+}
+
+void display(node *head)
+{
+  printf("\n");
+  while (head != NULL)
+  {
+    printf("%d ", head->data);
+    head = head->next;
+  }
+}
+
+node *reverse(node *head)
+{
+  node *curr = head;
+  node *after = NULL, *prev = NULL;
+  while (curr != NULL)
+  {
+    after = curr->next;
+    curr->next = prev;
+
+    prev = curr;
+    curr = after;
+  }
+  return prev;
+}
+
+int main()
+{
+  node *head = NULL;
+  int ch;
+  do
+  {
+    printf("\ninput the choice:\n1-create a link\n2-display\n3-revrese the linked list\n");
+    scanf("%d", &ch);
+    switch (ch)
+    {
+    case 1:
+      head = createlink(head);
+      break;
+    case 2:
+      display(head);
+      break;
+    case 3:
+      head = reverse(head);
+      break;
+    }
+
+  } while (ch != 0);
+}
