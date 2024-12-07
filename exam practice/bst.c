7029a7d036d3cbf8b4603dc5a78a56895bd66423
Achyut-shekhar <achyutshekhar54@gmail.com>
2024-12-07 22:08:50 +0530

    bst changes

---
+++ exam practice/bst.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+  int data;
+  struct node *left, *right;
+} node;
+
+node *create(int data)
+{
+  node *newnode = (node *)malloc(sizeof(node));
+  newnode->data = data;
+  newnode->left = NULL;
+  newnode->right = NULL;
+  return newnode;
+}
+
+node *insert(node *root, int data)
+{
+  if (root == NULL)
+  {
+    root = create(data);
+  }
+  if (data < root->data)
+  {
+    root->left = insert((root->left), data);
+  }
+  if (data > root->data)
+  {
+    root->right = insert((root->right), data);
+  }
+  return root;
+}
+
+void inOrder(node *root)
+{
+  if (root != NULL)
+  {
+    inOrder(root->left);
+    printf("%d->\t", root->data);
+    inOrder(root->right);
+  }
+}
+node *getSuccessor(node *root)
+{
+  root = root->right;
+  while (root != NULL && root->left != NULL)
+  {
+    root = root->left;
+  }
+  return root;
+}
+
+node *delete(node *root, int data)
+{
+  if (root == NULL)
+    return root;
+
+  // Navigate to the node to delete
+  if (root->data > data)
+    root->left = delete (root->left, data);
+  else if (root->data < data)
+    root->right = delete (root->right, data);
+  else
+  {
+    // Case 1: Node with no children (leaf node)
+    if (root->left == NULL && root->right == NULL)
+    {
+      free(root);
+      return NULL;
+    }
+
+    // Case 2: Node with one child (either left or right)
+    if (root->left == NULL) // Node with only right child
+    {
+      node *temp = root->right;
+      free(root);
+      return temp;
+    }
+    else if (root->right == NULL) // Node with only left child
+    {
+      node *temp = root->left;
+      free(root);
+      return temp;
+    }
+
+    // Case 3: Node with two children
+    node *succ = getSuccessor(root);                // Get the inorder successor
+    root->data = succ->data;                        // Replace root data with successor's data
+    root->right = delete (root->right, succ->data); // Delete the successor node
+  }
+  return root;
+}
+
+int main()
+{
+  struct node *root = NULL;
+
+  root = insert(root, 50);
+  root = insert(root, 30);
+  root = insert(root, 70);
+  root = insert(root, 20);
+  root = insert(root, 40);
+  root = insert(root, 60);
+  root = insert(root, 80);
+
+  printf("---------inorder-------\n");
+  inOrder(root);
+
+  root = delete (root, 40);
+
+  printf("\n------deleted bst--------\n");
+  inOrder(root);
+
+  return 0;
+}
